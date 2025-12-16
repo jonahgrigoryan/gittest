df5f06908d64087177c839089ba20fee503e3596
jonahgrigoryan <jonahkesoyan@aol.com>
2025-12-15 23:44:44 -0800

    fix: vendor protoc for solver build

--- services/solver/build.rs
+++ services/solver/build.rs
@@ -1,6 +1,12 @@
 fn main() {
     println!("cargo:rerun-if-changed=../../proto/solver.proto");
+
+    // Use a vendored protoc binary so CI machines do not need system-level protoc installed.
+    let protoc_path = protoc_bin_vendored::protoc_bin_path()
+        .expect("protoc binary should be available via protoc-bin-vendored");
+
     tonic_build::configure()
+        .protoc_path(protoc_path)
         .build_server(true)
         .compile(&["../../proto/solver.proto"], &["../../proto"])
         .unwrap();
